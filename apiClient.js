0ea19eb6f7d1e63b8dae7f0bbf89d91d82a45ef5
Saki Matsumoto <kinoko@ueo.co.jp>
2017-04-06 08:22:07 +0900

    step3 postメソッドの実装

--- apiClient.js
+++ apiClient.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
 });
 
 function dispMessages() {
+    $('#chat_box').empty();
+
     var messages = new Array();
 
     $.getJSON("http://153.126.169.85:8080/chat/MainServlet?",
@@ -21,10 +23,8 @@ function dispMessages() {
             for(var index = 0; index < messages.length; index++) {
                 var message = messages[index];
                 $('#chat_box').append('<div class="message_box"></div>');
-                $('.message_box').eq(index).append('<div class="user">なまえ</div>');
                 $('.message_box').eq(index).append('<div class="content">とうこうないよう</div>');
                 $('.content').eq(index).text(message.content);
-                $('.user').eq(index).text(message.messageOwner.name);
             }
         })
         .fail(function (jqxhr, textStatus, error) {
@@ -32,3 +32,63 @@ function dispMessages() {
             alert("Request Failed: " + err);
         });
 }
+
+function postMessage() {
+    var content = $('#post_content').val();
+    var createTime = Date.now();
+    var messageId = createNewMessageId();
+
+    var obj = {
+        lang : "ja",
+        token : "dummy",
+        request :
+        {
+            message:
+            {
+                messageId : messageId,
+                    messageOwner : {userId: "dummy", name: "John Doe"},
+                createTime : createTime,
+                    content : content
+            }
+        }
+    }
+    var jsonString = JSON.stringify(obj);
+    //alert(jsonString);
+
+    // 通信実行
+    $.ajax(
+        {
+            type: "post",                // method = "POST"
+            url: "http://153.126.169.85:8080/chat/MainServlet?api=Messages", // POST送信先のURL
+            data: jsonString,  // JSONデータ本体
+            contentType: "application/json;charset=UTF-8", // リクエストの Content-Type
+            dataType: "json" // レスポンスをJSONとしてパースする
+        }
+    ).done(function() {
+            //alert( "success" );
+            dispMessages();
+        }
+    ).fail(function() {
+            alert( "error" );
+        }
+    );
+}
+
+function createNewMessageId() {
+    // 生成する文字列の長さ
+    var l = 8;
+
+    // 生成する文字列に含める文字セット
+    var c = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    var cl = c.length;
+    var r = "";
+    for(var i=0; i<l; i++){
+        r += c[Math.floor(Math.random()*cl)];
+    }
+    var createTime = Date.now();
+    var messId = r + createTime;
+    //alert(messId);
+    return messId;
+}
+
